2e085dd354b74dea56546af2919bca2cd4f446e8
linfaxin <linlinfaxin@163.com>
2023-09-12 21:36:46 +0800

    fix: 忽略 HttpError 错误（认为请求已处理成功）

--- server/routes/bbs/init.ts
+++ server/routes/bbs/init.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { useExpressServer } from 'routing-controllers';
+import { HttpError, useExpressServer } from 'routing-controllers';
 import { Express } from 'express';
 import * as path from 'path';
 
@@ -9,5 +9,14 @@ export function initBBSRouter(app: Express) {
     controllers: [path.join(`${__dirname}/controllers/*Controller.ts`), path.join(`${__dirname}/controllers/*Controller.js`)],
     interceptors: [`${__dirname}/global-interceptors/*.ts`, `${__dirname}/global-interceptors/*.js`],
     cors: true,
+  });
+
+  app.use('/bbs', (err, req, res, next) => {
+    if (err instanceof HttpError && res.headersSent) {
+      // HttpError 已经由 routing-controllers 内部错误处理器 处理，无需向上抛给 express（会导致错误）
+      next();
+    } else {
+      next(err);
+    }
   });
 }
