f85c63046819ecd419d9c51c32b025b9dce5c7a0
linfaxin <linlinfaxin@163.com>
2024-06-06 23:08:48 +0800

    feat: 小图片无需压缩

--- web/src/utils/compress-image-util.ts
+++ web/src/utils/compress-image-util.ts
@@ -3,6 +3,10 @@ import Compressor from 'compressorjs';
 const CheckOrientationSizeLimit = 5 * 1024 * 1024;
 
 export function compressImageFile(data: Blob, option?: Compressor.Options): Promise<Blob> {
+  if (option?.maxWidth && option?.maxHeight && data.size < Math.max(option.maxWidth, option.maxHeight) * 100) {
+    // 小图 无需压缩
+    return Promise.resolve(data);
+  }
   return new Promise<Blob>((resolve, reject) => {
     new Compressor(data, {
       quality: 0.8,
